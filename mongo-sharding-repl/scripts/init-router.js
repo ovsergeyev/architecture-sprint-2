58eeca431ec201be3271d35f492675ff0abf8d94
O.V.Sergeyev <o.v.sergeyev@gmail.com>
2024-09-20 11:39:27 +0000

    settings

--- mongo-sharding-repl/scripts/init-router.js
+++ mongo-sharding-repl/scripts/init-router.js
@@ -1,5 +1,9 @@
-sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "rs-shard-01/shard01-a:27101");
+sh.addShard( "rs-shard-01/shard01-a:27102");
+sh.addShard( "rs-shard-01/shard01-a:27103");
+sh.addShard( "rs-shard-02/shard02-a:27201");
+sh.addShard( "rs-shard-02/shard02-a:27202");
+sh.addShard( "rs-shard-02/shard02-a:27203");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
--- mongo-sharding-repl/scripts/init-shard2.js
+++
@@ -1,8 +0,0 @@
-rs.initiate(
-  {
-    _id : "shard2",
-    members: [
-      { _id : 1, host : "shard2:27019" }
-    ]
-  }
-);
